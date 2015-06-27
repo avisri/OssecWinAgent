da78405bd45a7e669cb6b274700996d5fb5aa59f
Avinash Sridhar <avinash.sridhar@salesforce.com>
2015-06-27 01:58:38 -0700

    As is from the web , copied from - http://www.mediafire.com/download/18ol828ylzuyxp5/OssecWinAgentDeployment.zip

---
+++ README.txt
@@ -0,0 +1,14 @@
+For instructions on how to use these scripts to silently deploy OssecHIDS to Windows Agents Please go to http://philipshramko.blogspot.com/2010_10_01_archive.html.  There is a 3 part tutorial outlining the entire process.  The tutorial is also posted at the ossec-list google groups page in the following posts.  
+
+Part 1:
+http://groups.google.com/group/ossec-list/browse_thread/thread/17c3f0103f37076f
+
+Part 2:
+http://groups.google.com/group/ossec-list/browse_thread/thread/73bf80f3e7cbcaaf
+
+Part 3:
+http://groups.google.com/group/ossec-list/browse_thread/thread/424c16ad860bd7b0
+
+I hope you will find this information useful and informative.
+
+Thank you.
---
+++ post install config scripts_CORRECT FILE NEEDED FOR WIN32 OR WIN64/ossec_distribute_keys_x64.cmd
@@ -0,0 +1,85 @@
+@ECHO OFF
+
+::*****************************************
+::* SET YOUR OSSEC SERVER IP ADDRESS HERE *
+::*****************************************
+SET OSSECSERVER=***Ossec Server IP here***
+
+::*******************************************************************************************
+::* DEFINE OSSECPATH VARIABLE AS SET BY THE Ossec HIDS INSTALLATION IN THE WINDOWS REGISTRY *
+::*******************************************************************************************
+SETLOCAL ENABLEDELAYEDEXPANSION
+FOR /f "tokens=1-2,*" %%i in ('reg query HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\ossec
+^| find "Install_Dir"') do SET OSSECPATH=%%k
+IF "!OSSECPATH:~-1!"==" " SET OSSECPATH="!OSSECPATH:~0,-1!"
+
+::*******************************************************************************
+::* VERIFY THAT OSSEC INSTALLED SUCCESSFULLY BEFORE PROCEEDING TO CONFIGURATION *
+::*******************************************************************************
+IF DEFINED OSSECPATH (ECHO Ossec installation found at !OSSECPATH!. Continuing with configuration.)
+IF NOT DEFINED OSSECPATH GOTO EXIT_ON_ERROR
+
+::***********************************
+::* CHECK FOR MASTER CLIENT KEYFILE *
+::***********************************
+IF EXIST "%CD%\ossec.keys" ECHO Client keyfile index found - Getting security configuration information.
+IF NOT EXIST "%CD%\ossec.keys" GOTO MASTER_KEYFILE_NOT_FOUND
+
+::*************************************
+::* ENABLE DELAYED VARIABLE EXPANSION *
+::*************************************
+@ECHO OFF > %OSSECPATH%\ossec.keys & SETLOCAL ENABLEDELAYEDEXPANSION
+
+::******************************************************************************************
+::* FIND COMPUTERNAME IN  MASTER KEYFILE THEN GENERATE SPECIFIC AGENT KEYFILE IN OSSECPATH *
+::******************************************************************************************
+findstr /I /C:"%COMPUTERNAME%" ossec.keys > "%OSSECPATH%\client.keys"
+
+::******************************************************************
+::* CONFIGURE ossec.conf FILE BEFORE RESTARTING OSSEC HIDS SERVICE *
+::******************************************************************
+SET LINE1=^<ossec_config^>
+SET LINE2=  ^<client^>
+SET LINE3=   ^<server-ip^>%OSSECSERVER%^</server-ip^>
+SET LINE4=  ^</client^>
+SET LINE5=^</ossec_config^>
+
+ECHO !LINE1!>>"%OSSECPATH%\ossec.conf"
+ECHO !LINE2!>>"%OSSECPATH%\ossec.conf"
+ECHO !LINE3!>>"%OSSECPATH%\ossec.conf"
+ECHO !LINE4!>>"%OSSECPATH%\ossec.conf"
+ECHO !LINE5!>>"%OSSECPATH%\ossec.conf"
+
+::**********************************************
+::* Restart Ossec Service and begin monitoring *
+::**********************************************
+NET STOP OSSECSVC
+NET START OSSECSVC
+
+::*************************************************************************
+::* NOTIFY USERS/TECHS THAT INSTALLATION AND CONFIGURATION WAS SUCCESSFUL *
+::*************************************************************************
+MSG * Ossec HIDS installation and configuration completed successfully. Click OK to EXIT
+
+::****************************************************************
+::* DELETE SENSITIVE FILES ONCE INSTALL IS COMPLETE AND VERIFIED *
+::****************************************************************
+DEL "%OSSECPATH%\ossec.keys"
+DEL "%OSSECPATH%\ossec_distribute_keys_x64.cmd"
+EXIT
+
+
+::***************************************************
+::* NOTIFY USERS/TECHS IF OSSEC INSTALLATION FAILED *
+::***************************************************
+:EXIT_ON_ERROR
+MSG * Ossec installation Failed.  Please contact support for assistance.
+EXIT
+
+
+::*****************************************************
+::* NOTIFY USERS/TECHS IF MASTER KEYFILE IS NOT FOUND *
+::*****************************************************
+:MASTER_KEYFILE_NOT_FOUND
+MSG * Client keyfile not found - Manual security configuration required.  Please contact support for assistance.
+EXIT
